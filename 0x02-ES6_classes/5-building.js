6f44dad143a471da7ba4b1e5608ab4412b0c74d0
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-03-08 21:13:13 +0200

    A Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,24 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+
+    this._sqft = sqft;
+
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = newSqft;
+  }
+}
