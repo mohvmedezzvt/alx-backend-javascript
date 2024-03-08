75c2bfbf2213fbb20ef5a47c02e5bee4d136a75d
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-03-08 20:41:13 +0200

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,23 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    }
+
+    for (const student of students) {
+      if (typeof student !== 'string') {
+        throw new TypeError('Students must be an array of strings');
+      }
+    }
+
     this._name = name;
     this._length = length;
     this._students = students;
@@ -7,14 +25,6 @@ export default class HolbertonCourse {
 
   get name() {
     return this._name;
-  }
-
-  get length() {
-    return this._length;
-  }
-
-  get students() {
-    return this._students;
   }
 
   set name(newName) {
@@ -25,12 +35,20 @@ export default class HolbertonCourse {
     }
   }
 
+  get length() {
+    return this._length;
+  }
+
   set length(newLength) {
     if (typeof newLength === 'number') {
       this._length = newLength;
     } else {
       throw new TypeError('Length must be a number');
     }
+  }
+
+  get students() {
+    return this._students;
   }
 
   set students(newStudents) {
