2052f6a37a4d8117da83adf5d53b04110a91b84b
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-03-08 22:02:29 +0200

    Hoisting

---
+++ 0x02-ES6_classes/9-main.js
@@ -0,0 +1,9 @@
+import { listOfStudents, HolbertonClass, StudentHolberton } from './9-hoisting';
+
+console.log(listOfStudents);
+
+const listPrinted = listOfStudents.map(
+  (student) => student.fullStudentDescription,
+);
+
+console.log(listPrinted);
