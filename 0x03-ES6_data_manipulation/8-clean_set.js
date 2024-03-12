31a57d079446fa702bea4b7ab6c3e71dd2e1c85b
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-03-12 23:57:07 +0200

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (startString === '' || typeof startString !== 'string' || !(set instanceof Set)) return '';
-  return Array.from(set).filter((value) => value.startsWith(startString)).map((value) => value.slice(startString.length)).join('-');
+  if (!startString || typeof startString !== 'string' || !(set instanceof Set)) return '';
+  const result = [];
+  for (const item of set) {
+    if (typeof item === 'string' && item.startsWith(startString)) {
+      result.push(item.substring(startString.length));
+    }
+  }
+  return result.join('-');
 }
