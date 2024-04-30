5ee0fb5118f19298568a05c276f3d0817f5c91ca
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-05-01 01:22:12 +0300

    Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line consistent-return
+    fs.readFile(path, (err, dataBuffer) => {
+      if (err) {
+        return reject();
+      }
+      const data = dataBuffer.toString().split('\n');
+      let count = 0;
+      const fields = {};
+
+      const firstnameIndex = data[0].split(',').indexOf('firstname');
+      const fieldIndex = data[0].split(',').indexOf('field');
+      // eslint-disable-next-line no-plusplus
+      for (let i = 1; i < data.length; i++) {
+        // eslint-disable-next-line no-continue
+        if (data[i] === '') continue;
+        // eslint-disable-next-line no-plusplus
+        count++;
+        const row = data[i].split(',');
+        if (fields[row[fieldIndex]]) {
+          fields[row[fieldIndex]].push(row[firstnameIndex]);
+        } else {
+          fields[row[fieldIndex]] = [row[firstnameIndex]];
+        }
+      }
+      console.log(`Number of students: ${count}`);
+      // eslint-disable-next-line guard-for-in
+      for (const field in fields) {
+        console.log(
+          `Number of students in ${field}: ${fields[field].length}. List: ${fields[
+            field
+          ].join(', ')}`,
+        );
+      }
+      resolve();
+    });
+  }).catch(() => {
+    throw new Error('Cannot load the database');
+  });
+}
+
+module.exports = countStudents;
